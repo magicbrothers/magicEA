fb49c1df496dc477abb31a0abcc016e31aab8458
magicbrothers <felix@felix-zauberer.de>
2020-05-04 15:02:47 +0200

    [+] messages in DE

---
+++ src/ui/lang/de.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+	"setup_dbhost" => "DB-Host",
+	"setup_dbuser" => "DB-Nutzer",
+	"setup_dbpasswd" => "DB-Passwort",
+	"setup_db" => "Datenbank",
+	"setup_button" => "Einrichten!",
+	"index_title" => "Startseite",
+	"index_welcome" => "Willkommen zu magicEA. Hier kannst du deine Bans und Mutes einsehen und einen Entbannungs/-entmutungsantrag stellen.",
+	"input_uuid" => "UUID",
+	"input_code" => "Code",
+	"login_button" => "Login",
+	"index_loggedin" => "Hallo %player%!",
+	"logout" => "Ausloggen"
+);
